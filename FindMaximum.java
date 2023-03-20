dab7484bfe5ffc488ad0f3911379734aaa378058
sanjana8100 <sanjanams812@gmail.com>
2023-03-20 17:43:07 +0530

    added: UC2

--- FindMaximum.java
+++ FindMaximum.java
@@ -9,8 +9,16 @@ public class FindMaximum {
         System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
     }
 
+    public static void maximumOfFloat(Float x, Float y, Float z){
+        Float max=x;
+        if((y.compareTo(max))>0) max=y;
+        if((z.compareTo(max))>0) max=z;
+        System.out.println("Maximum of "+x+", "+y+" and "+z+" is: "+max);
+    }
+
     public static void main(String[] args) {
         Integer int1= 3, int2= 4, int3= 5;
+        Float flt1=6.6f, flt2=8.8f, flt3=7.7f;
 
         //TestCase1.1
         maximumOfInteger(int3,int1,int2);
@@ -18,6 +26,15 @@ public class FindMaximum {
         maximumOfInteger(int1,int3,int2);
         //TestCase1.3
         maximumOfInteger(int1,int2,int3);
+
+        System.out.println();
+
+        //TestCase2.1
+        maximumOfFloat(flt2,flt1,flt3);
+        //TestCase2.2
+        maximumOfFloat(flt1,flt2,flt3);
+        //TestCase2.3
+        maximumOfFloat(flt1,flt3,flt2);
 
     }
 }
